e3732230c96b336737a1e93fc1202468fd8405f9
thomaskovoor <thomaskovoor123@gmail.com>
2023-11-07 21:34:53 +0530

    connected to mysql and added repository

---
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.thomas.Bank.Application.repository;
+
+import com.thomas.Bank.Application.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
