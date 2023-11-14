823faa0f2a6eba040b37e69a9ebde550dafed023
thomaskovoor <thomaskovoor123@gmail.com>
2023-11-15 02:36:52 +0530

    feat: add api endpoints for nameEnquiry and balanceEnquiry
    add controllers for nameEnquiry and balanceEnquiry

--- Bank-Application/src/main/java/com/thomas/Bank/Application/controller/UserController.java
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.thomas.Bank.Application.controller;
 
 import com.thomas.Bank.Application.dto.BankResponse;
+import com.thomas.Bank.Application.dto.EnquiryRequest;
 import com.thomas.Bank.Application.dto.UserReq;
 import com.thomas.Bank.Application.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,12 @@ public class UserController {
     public BankResponse createAccount(@RequestBody UserReq userReq){
         return userService.createAccount(userReq);
     }
+
+    @GetMapping("/api/balanceEnquiry")
+    public BankResponse balanceEnquiry(@RequestBody EnquiryRequest enquiryRequest){ return userService.balanceEnquiry(enquiryRequest);}
+
+    @GetMapping("/api/nameEnquiry")
+    public String nameEnquiry(@RequestBody EnquiryRequest enquiryRequest){ return userService.nameEnquiry(enquiryRequest);}
+
 
 }
