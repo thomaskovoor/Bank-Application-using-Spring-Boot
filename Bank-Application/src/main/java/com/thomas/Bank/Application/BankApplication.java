0da0b20633f1104948edd458bb4e37aa98a0596c
thomaskovoor <thomaskovoor123@gmail.com>
2023-11-20 02:03:09 +0530

    docs: add title details on documentation in Swagger UI

--- Bank-Application/src/main/java/com/thomas/Bank/Application/BankApplication.java
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/BankApplication.java
@@ -1,9 +1,35 @@
 package com.thomas.Bank.Application;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Bank Application",
+				description = "Summary about the APIs used in Bank Application",
+				version = "1.0",
+				contact = @Contact(
+						name = "Thomas Kovoor",
+						email = "thomaskovoor123@gmail.com",
+                        url = "https://github.com/thomaskovoor/Bank-Application-using-Spring-Boot"
+				),
+                license = @License(
+						name = "Bank Application License",
+						url = "https://github.com/thomaskovoor/Bank-Application-using-Spring-Boot"
+				),
+				summary = "Details about APIs"
+		) ,
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Application Documentation",
+				url = "https://github.com/thomaskovoor/Bank-Application-using-Spring-Boot"
+		)
+)
 public class BankApplication {
 
 	public static void main(String[] args) {
