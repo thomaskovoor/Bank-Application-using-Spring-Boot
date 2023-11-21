bd02538611849e23a789b7749d5a25d0401bf541
thomaskovoor <thomaskovoor123@gmail.com>
2023-11-21 19:40:04 +0530

    docs: add schema details on documentation in Swagger UI

--- Bank-Application/src/main/java/com/thomas/Bank/Application/dto/AccountInfo.java
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/dto/AccountInfo.java
@@ -1,5 +1,6 @@
 package com.thomas.Bank.Application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,10 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountInfo {
+    @Schema(name = "User Account Name")
     private String accountName;
+    @Schema(name = "User Account Number")
     private String accountNumber;
+    @Schema(name = "User Account Balance")
     private BigDecimal accountBalance;
 }
