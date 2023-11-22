4ccd8c8c1b9de2050a454ab937872b5d0073436d
thomaskovoor <thomaskovoor123@gmail.com>
2023-11-22 18:48:30 +0530

    fix: update type of createdAt and modifiedAt in Transaction entity from String to LocalDateTime

--- Bank-Application/src/main/java/com/thomas/Bank/Application/entity/Transaction.java
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/entity/Transaction.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -23,7 +24,7 @@ public class Transaction {
     private String accountNumber;
     private String status;
     @CreationTimestamp
-    private String createdAt;
+    private LocalDateTime createdAt;
     @UpdateTimestamp
-    private String modifiedAt;
+    private LocalDateTime modifiedAt;
 }
