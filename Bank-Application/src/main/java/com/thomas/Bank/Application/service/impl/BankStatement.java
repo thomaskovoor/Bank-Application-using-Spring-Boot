66dcfaf77ee6e48aad578d73f1346fd6b797ead9
thomaskovoor <thomaskovoor123@gmail.com>
2023-11-23 01:52:01 +0530

    fix: fix error in transactionList
    returns correct list between the two dates

--- Bank-Application/src/main/java/com/thomas/Bank/Application/service/impl/BankStatement.java
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/service/impl/BankStatement.java
@@ -3,22 +3,29 @@ package com.thomas.Bank.Application.service.impl;
 
 import com.thomas.Bank.Application.entity.Transaction;
 import com.thomas.Bank.Application.repository.TransactionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Component
+@AllArgsConstructor
 public class BankStatement {
 
     private TransactionRepository transactionRepo;
     public List<Transaction> generateBankStatement(String accountNumber, String fromDate, String toDate){
 
-        LocalDate startDate = LocalDate.parse(fromDate, DateTimeFormatter.ISO_LOCAL_DATE);
-        LocalDate endDate = LocalDate.parse(toDate,DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate startDate = LocalDate.parse(fromDate, DateTimeFormatter.ISO_DATE);
+        LocalDate endDate = LocalDate.parse(toDate,DateTimeFormatter.ISO_DATE);
 
         return transactionRepo.findAll().stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-                .filter(transaction -> transaction.getCreatedAt().isEqual(startDate))
-                .filter(transaction -> transaction.getCreatedAt().isEqual(endDate)).toList();
+                .filter(transaction -> transaction.getCreatedAt().isAfter(startDate) && transaction.getCreatedAt().isBefore(endDate))
+                .toList();
+
     }
 
 }
