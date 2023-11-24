1d3328dabe269a3010f4ae73ba6367dda7694605
thomaskovoor <thomaskovoor123@gmail.com>
2023-11-24 22:18:52 +0530

    fix: return transactions from startDate to endDate inclusive

--- Bank-Application/src/main/java/com/thomas/Bank/Application/service/impl/BankStatement.java
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/service/impl/BankStatement.java
@@ -7,10 +7,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @AllArgsConstructor
@@ -19,11 +20,19 @@ public class BankStatement {
     private TransactionRepository transactionRepo;
     public List<Transaction> generateBankStatement(String accountNumber, String fromDate, String toDate){
 
-        LocalDate startDate = LocalDate.parse(fromDate, DateTimeFormatter.ISO_DATE);
-        LocalDate endDate = LocalDate.parse(toDate,DateTimeFormatter.ISO_DATE);
+        //parsing string date to local date format yy-mm-dd and
+        //converting it to local date time yy-mm-dd-h:m:s
+        LocalDateTime startDate = LocalDate.parse(fromDate,DateTimeFormatter.ISO_DATE).atTime(LocalTime.MIN);//00:00:00
+        LocalDateTime endDate = LocalDate.parse(toDate,DateTimeFormatter.ISO_DATE).atTime(LocalTime.MAX);//23:59:59
 
+        //returning all transactions of specified account number from start date to end date
+        // including both start and end date
         return transactionRepo.findAll().stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-                .filter(transaction -> transaction.getCreatedAt().isAfter(startDate) && transaction.getCreatedAt().isBefore(endDate))
+                .filter(transaction ->
+                        (transaction.getCreatedAt().isEqual(startDate) || transaction.getCreatedAt().isAfter(startDate))
+                                && (transaction.getCreatedAt().isEqual(endDate) || transaction.getCreatedAt().isBefore(endDate))
+                       )
+                .sorted(Comparator.comparing(Transaction::getCreatedAt))
                 .toList();
 
     }
