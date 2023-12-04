31288a132ae6d78d7b06bc91676647d7af3fcee7
thomaskovoor <thomaskovoor123@gmail.com>
2023-12-05 02:42:52 +0530

    fix: disable authentication for creating user
    disable authentication for user endpoint

--- Bank-Application/src/main/java/com/thomas/Bank/Application/config/SecurityConfig.java
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.thomas.Bank.Application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +21,20 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        //disabling authentication for creating user endpoint
+
+        httpSecurity.csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(authorize->
+                        authorize.requestMatchers(HttpMethod.POST,"/api/user").permitAll()
+                                .anyRequest().authenticated());
+
+        httpSecurity.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+
+    }
 
 
 }
