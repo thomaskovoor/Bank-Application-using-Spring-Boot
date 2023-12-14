e60cba04702a9c12580760ce600a16719ede6e70
thomaskovoor <thomaskovoor123@gmail.com>
2023-12-15 03:08:10 +0530

    feat: Add JwtTokenProvider class for JWT generation
    generateToken method creates a JWT with user-related information and signs it using the key method, which generates the cryptographic key

---
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/config/JwtTokenProvider.java
@@ -0,0 +1,64 @@
+package com.thomas.Bank.Application.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecretKey;
+    @Value("${app.jwt-expiration}")
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication){
+       String userName = authentication.getName();
+        Date currentDate = new Date();
+        Date expiryDate = new Date(currentDate.getTime() +jwtExpirationDate);
+
+        return Jwts.builder()
+                .subject(userName)
+                .issuedAt(currentDate)
+                .expiration(expiryDate)
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key() {
+        byte[] bytes = Decoders.BASE64.decode(jwtSecretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+    public String getUserName(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser()
+                    .setSigningKey(key())
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (ExpiredJwtException | IllegalArgumentException | SecurityException | MalformedJwtException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
