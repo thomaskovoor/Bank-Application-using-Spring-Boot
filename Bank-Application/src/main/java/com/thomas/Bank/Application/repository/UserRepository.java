78529e917f05c743ea289247ba3e79448ca865a8
thomaskovoor <thomaskovoor123@gmail.com>
2023-12-16 14:49:55 +0530

    fix: add loadByEmail function in respository and import changes

--- Bank-Application/src/main/java/com/thomas/Bank/Application/repository/UserRepository.java
+++ Bank-Application/src/main/java/com/thomas/Bank/Application/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.thomas.Bank.Application.repository;
 import com.thomas.Bank.Application.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
    Boolean existsByEmail(String email);
-
    Boolean existsByAccountNumber(String accountNumber);
-
    User findByAccountNumber(String accountNumber);
+   Optional<User> findByEmail(String email);
 }
